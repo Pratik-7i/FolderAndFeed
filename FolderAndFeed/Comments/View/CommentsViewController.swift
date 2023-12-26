3782209feecabbb29a56a81648f8675aab0cf4b2
Pratik Prajapati <96768526+Pratik-7i@users.noreply.github.com>
2023-12-26 14:21:04 +0530

    End Test

--- FolderAndFeed/Comments/View/CommentsViewController.swift
+++ FolderAndFeed/Comments/View/CommentsViewController.swift
@@ -59,8 +59,6 @@ extension CommentsViewController: UITableViewDataSource {
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let comment = viewModel.comments[section]
-        print("")
-        print("Roes: \(comment.replies.count + 1)")
         return comment.replies.count + 1
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
